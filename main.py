6e8aaa9b5071bf1749a8a4dd4d994a8ab2c02c70
sandbagimon <wzy5228025@gmail.com>
2024-06-05 17:16:45 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+from autogen_test import group_chat_init
+
+st.title('Professors and papers')
+
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    # temp_file_path = os.path.join("tempDir", uploaded_file.name)
+    temp_file_path = os.path.join("tempDir", 'temp.pdf')
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        file_name = uploaded_file.name
+
+    with st.container():
+        group_chat_init(temp_file_path)
+
+    # df = group_chat_init("tempDir\\"+uploaded_file.name)
+
+
+
